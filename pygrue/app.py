3ea16591a167395ed340ee310ca224b70557096c
David Freifelder <dfreifelder@comcast.net>
2023-12-19 10:42:21 -0800

    Sort the character entries

--- pygrue/app.py
+++ pygrue/app.py
@@ -6,12 +6,12 @@ from pygrue.ui import UI
 
 class App:
     __slots__ = {
-        "pcs": "A set of the Character objects tracked"
+        "pcs": "A list of the Character objects tracked"
     }
 
     def __init__(self, character_dir: pathlib.Path) -> None:
-        self.pcs = set([
+        self.pcs = [
             PC(next_char)
             for next_char in character_dir.iterdir()
-        ])
+        ]
         UI(self.pcs)
